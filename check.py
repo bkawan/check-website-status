8d0f91d5de5370ed3fff2a300e1c575cd45802a0
bkawan <bikeshkawang@gmail.com>
2018-01-14 11:54:02 +0545

    initial commit check website status

---
+++ check.py
@@ -0,0 +1,107 @@
+import sqlite3
+
+import requests
+
+template = '''<!DOCTYPE html>
+<html>
+<head>
+<style>{}</style>
+
+</head>
+<body>
+<p>
+Hello {},
+<br>
+Below is the detail of your property.
+</p>
+
+<table style="width:100%">
+  <tr>
+    <th>Website</th>
+    <th>Status</th>
+    <th>Website Load Time(Seconds)</th>
+  </tr>
+  {}
+</table>
+</body>
+</html>
+
+'''
+
+table_row = '''
+ <tr>
+    <td>{}</td>
+    <td>{}</td>
+    <td>{}</td>
+  </tr>
+'''
+
+style = '''table { border-collapse: collapse; }
+table th { background-color: #C6E0B4; }
+table th, table td { padding: 5px; border:1px solid black}'''
+
+admin_emails = ['bikeshkawang@gmail.com', 'lipipoint@gmail.com']
+conn = sqlite3.connect('web.db')
+c = conn.cursor()
+c.execute('SELECT * FROM website ORDER BY id')
+rows = c.fetchall()
+all_rows = ''
+
+
+def check_website_status_and_load_time(url):
+    response = requests.get(url)
+    status_code = response.status_code
+    load_time = response.elapsed.total_seconds()
+    return {
+        'status':status_code,
+        'load_time':load_time
+    }
+
+
+def send_email(recp, subject, body):
+    import smtplib
+    from email.header import Header
+    from email.utils import formataddr
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+    TO = recp
+    # Gmail Sign In
+    gmail_sender = 'gmail'
+    gmail_passwd = 'password'
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = gmail_sender
+    msg['To'] = TO
+    body = MIMEText(body, 'html')
+    msg.attach(body)
+    author = formataddr((str(Header(u'{}', 'utf-8')), gmail_sender))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(gmail_sender, gmail_passwd)
+    try:
+        server.sendmail(gmail_sender, [TO], msg.as_string())
+        print('email sent to {}'.format(recp))
+    except:
+        print('error sending mail'.format(recp))
+    server.quit()
+
+
+if __name__ == "__main__":
+
+    for row in rows:
+        website = row[1]
+        manager_email = row[2]
+        status_and_load_time = check_website_status_and_load_time(website)
+        status = status_and_load_time['status']
+        load_time = status_and_load_time['load_time']
+        subject = 'Website status and load time for {} '.format(website)
+        _table_row = table_row.format(website, status, load_time)
+        all_rows += _table_row
+        body = template.format(style, manager_email, _table_row)
+        send_email(manager_email, subject, body)
+
+    # Send Email to admins
+    for email in admin_emails:
+        send_email(email, 'All Website Status', template.format(style, email, all_rows))
