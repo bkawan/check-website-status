d258dfdc96f938f28ed0923550df35e19362160d
bkawan <bikeshkawang@gmail.com>
2018-02-16 23:51:37 +0545

    fix website 403 forbidden by add user agent

--- check.py
+++ check.py
@@ -73,7 +73,9 @@ all_table_rows = ''
 
 def check_website_status_and_load_time(url):
     try:
-        response = requests.get(url)
+        headers = {
+            'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        response = requests.get(url, headers=headers)
         status_code = response.status_code
         load_time = response.elapsed.total_seconds()
     except:
